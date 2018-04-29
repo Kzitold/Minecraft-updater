909ee894f76611a1ed00c37d2c27ae822bd6bacb
Kzitold <austinleesmith1994@gmail.com>
2018-04-29 09:34:41 -0400

    Add files via upload

---
+++ Update/scripts/CurseForge.ps1
@@ -0,0 +1,39 @@
+if (Test-Path "$path\scripts\updater"){Remove-Item $path\scripts\updater -recurse}
+New-Item $path\scripts\updater -type directory -Force | Out-Null
+cd $path\scripts\updater
+$client = new-object System.Net.WebClient
+
+$mod = "https://minecraft.curseforge.com/projects/$mod/files"
+
+$client.DownloadFile("$mod/files","$path\scripts\updater\source.txt")
+
+Select-String source.txt -pattern "  $mv</option>" | Select-Object LineNumber > num.txt
+$num = get-content num.txt | select -Last 3 | select -First 1
+$ver = get-content source.txt | select -first 1 -skip ([int]$num-3)
+$ver = $ver.Remove(0,11)
+$ver = $ver.Remove($ver.Length-1)
+$mod = "$mod`?filter-game-version=$ver"
+
+$client.DownloadFile($mod,"$path\scripts\updater\source.txt")
+
+Select-String source.txt -pattern "col-downloads" | Select-Object LineNumber > num.txt
+$num = get-content num.txt | select -Last 3 | select -First 1
+$ver = get-content source.txt | select -first 1 -skip ([int]$num+28)
+
+$ver.split('"') > source.txt
+Select-String source.txt -pattern "href" | Select-Object LineNumber > num.txt
+$num = get-content num.txt | select -Last 3 | select -First 1
+$ver = get-content source.txt | select -first 1 -skip ([int]$num)
+
+$client.DownloadFile("https://minecraft.curseforge.com$ver","$path\scripts\updater\source.txt")
+
+Select-String source.txt -pattern "Filename" | Select-Object LineNumber > num.txt
+$num = get-content num.txt | select -Last 3 | select -First 1
+$jar = get-content source.txt | select -first 1 -skip ([int]$num)
+$jar = $jar.split(">") | select -first 1 -skip 1
+$jar = $jar.split("<") | select -first 1
+
+$client.DownloadFile("https://minecraft.curseforge.com$ver/download","$dir\mods\$mc\$jar")
+
+echo "$jar"
+cd $path
---
+++ Update/scripts/LiteLoader.ps1
@@ -0,0 +1,20 @@
+if (Test-Path "$path\scripts\updater"){Remove-Item $path\scripts\updater -recurse}
+New-Item $path\scripts\updater -type directory -Force | Out-Null
+$client = new-object System.Net.WebClient
+$jar = "liteloader-installer-$mc-00-SNAPSHOT.jar"
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+$client.DownloadFile("http://jenkins.liteloader.com/job/LiteLoaderInstaller%20$mc/lastSuccessfulBuild/artifact/build/libs/$jar","$path\scripts\updater\$jar")
+
+Unzip "$path\scripts\updater\$jar" "$path\scripts\updater\liteloader-$mc"
+Copy-Item $path\scripts\updater\liteloader-$mc\liteloader-$mc-SNAPSHOT-release.jar $dir\mods\$mc\liteloader-$mc-SNAPSHOT-release.jar -recurse
+
+echo "liteloader-$mc-SNAPSHOT-release.jar"
+cd $path
