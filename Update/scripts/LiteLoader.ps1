3557295d1fef15f56dd1052327eaaeaa2cf08944
Kzitold <austinleesmith1994@gmail.com>
2018-10-01 07:42:02 -0400

    Update LiteLoader.ps1

--- Update/scripts/LiteLoader.ps1
+++ Update/scripts/LiteLoader.ps1
@@ -1,6 +1,9 @@
-New-Item $path\scripts\updater -type directory -Force | Out-Null
+$host.UI.RawUI.WindowTitle = "$profile : LiteLoader"
+
+$updater = "$path\scripts\updater"
+New-Item $updater -type directory -Force | Out-Null
 $client = new-object System.Net.WebClient
-$jar = "liteloader-installer-$mc-00-SNAPSHOT.jar"
+$jar = "liteloader-installer-$mv-00-SNAPSHOT.jar"
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Unzip
@@ -10,11 +13,11 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-$client.DownloadFile("http://jenkins.liteloader.com/job/LiteLoaderInstaller%20$mc/lastSuccessfulBuild/artifact/build/libs/$jar","$path\scripts\updater\$jar")
+$client.DownloadFile("http://jenkins.liteloader.com/job/LiteLoaderInstaller%20$mv/lastSuccessfulBuild/artifact/build/libs/$jar","$updater\$jar")
 
-Unzip "$path\scripts\updater\$jar" "$path\scripts\updater\liteloader-$mc"
-Copy-Item $path\scripts\updater\liteloader-$mc\liteloader-$mc-SNAPSHOT-release.jar $dir\mods\$mc\liteloader-$mc-SNAPSHOT-release.jar -recurse
+Unzip "$updater\$jar" "$updater\liteloader-$mv"
+Copy-Item $updater\liteloader-$mv\liteloader-$mv-SNAPSHOT-release.jar $dir\mods\$mc\liteloader-$mv-SNAPSHOT-release.jar -recurse
 
-echo "liteloader-$mc-SNAPSHOT-release.jar"
+Remove-Item $updater -recurse
+echo "liteloader-$mv-SNAPSHOT-release.jar"
 cd $path
-Remove-Item $path\scripts\updater -recurse
